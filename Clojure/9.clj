016011ddcbd75f89193372015bc4d63bc2971e8c
Iker Lissarrague Berumen <ikerlb@pop-os.localdomain>
2021-09-22 16:47:42 -0500

    9 in clojure

---
+++ Clojure/9.clj
@@ -0,0 +1,28 @@
+;; a < b < c
+;; a * a + b * b = c * c
+;; a + b + c = 1000
+
+;; c = 1000 - b - a
+;; a² + b² = (1000 - b - a)²
+(defn get-b
+  [a]
+  (/ (* 1000 (- a 500)) (- a 1000)))
+
+(defn get-c
+  [a b]
+  (- (- 1000 b) a))
+
+(defn sum
+  [a & more]
+  (reduce + a more))
+
+;; if we fix a, we can
+;; calculate both b and c
+;; the only naturals
+;; should be the answer
+(filter
+  #(and (apply < (cons 0 %)) (every? int? %))
+  (for [a (range 1000)]
+    (let [b (get-b a)
+          c (get-c b a)]
+      [a b c])))
